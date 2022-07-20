030aaeda35858dca97138dab1dd4c804656306de
Rick <rsulisthio@gmail.com>
2022-07-20 10:40:57 +0700

    build: project config

---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+const Theme = {
+    colors: {
+        primary : '#5D5FEF',
+        danger : '#EB5757',
+        success : '#27AE60',
+        background: '#0D1B2A',
+        black: '#000000',
+        white: '#FFFFFF',
+        gray: '#989898'
+    }
+};
+
+export default Theme;
