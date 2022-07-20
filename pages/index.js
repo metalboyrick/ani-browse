6d91978979d95f38de180afccffca191b444e879
Rick <rsulisthio@gmail.com>
2022-07-20 13:54:07 +0700

    feat: add anime homepage style

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,14 @@
 
 import Head from "next/head";
 import Theme from "../styles/theme";
+import {Input} from "antd";
 
+// placeholder for home screen banners, to be changed with real server data later
+const banners = [
+    "https://s4.anilist.co/file/anilistcdn/media/anime/banner/20-HHxhPj5JD13a.jpg",
+    "https://s4.anilist.co/file/anilistcdn/media/anime/banner/21-wf37VakJmZqs.jpg",
+    "https://s4.anilist.co/file/anilistcdn/media/anime/banner/22-wVJjA9tGMt4k.jpg"
+];
 
 export default function Home() {
     return (
@@ -15,14 +22,64 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            
+            {/* Header Section */}
+            <div css={{
+                width: "100vw",
+                height: "40vh"
+            }}>
 
-            <h1
-                css={{
-                    color: Theme.colors.primary
-                }}
-            >
-                Hello This is AniBrowse
-            </h1>
+                <div
+                    css={{
+                        width: "100vw",
+                        height: "40vh",
+                        backgroundImage: `url(${banners[0]})`,
+                        backgroundSize: "cover",
+                        opacity: 0.35,
+                        position: "absolute"
+                    }}
+                />
+
+                <div css={{
+                    position: "absolute",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "left",
+                    justifyContent: "center",
+                    width: "100%",
+                    height: "40vh",
+                    paddingLeft: "20%",
+                    paddingRight: "20%"
+                }}>
+                    <h1 css={{
+                        color: Theme.colors.white,
+                        fontWeight: "bold",
+                        lineHeight: "1rem"
+                    }}>Hi, Welcome to AniBrowse!</h1>
+                    <p css={{
+                        fontWeight: 200,
+                        lineHeight: "1rem"
+                    }}>Explore your favourite anime.</p>
+                    
+                    {/* search bar */}
+                    <Input css={{
+                        width: "60%"
+                    }}placeholder="What are you looking for?"></Input>
+
+                    <p css={{
+                        fontWeight: 200,
+                        lineHeight: "2rem"
+                    }}>or <a css={{ 
+                        fontWeight: "bold", 
+                        "&:hover": { 
+                            color: Theme.colors.primary 
+                            } }}>Add to Collections</a></p>
+                </div>
+            </div>
+
+            {/* Anime cards and pagination */}
+            
+
         </div>
     );
 }
